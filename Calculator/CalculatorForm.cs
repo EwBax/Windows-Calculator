dbec786f6821525154651ab8b135b5b641102168
EwBax <W0465255@campus.nscc.ca>
2023-03-18 19:49:53 -0300

    Finished, might add comments still

--- Calculator/CalculatorForm.cs
+++ Calculator/CalculatorForm.cs
@@ -19,7 +19,8 @@ namespace Calculator
     public partial class CalculatorForm : Form
     {
         
-        private const int MAX_LENGTH = 14;
+        private const int MAX_DIGITS = 14;
+        private const int MAX_STR_LENGTH = 16;
 
         private double _leftNum;
         private double _rightNum;
@@ -34,6 +35,10 @@ namespace Calculator
 
         private void numBtn_Click(object sender, EventArgs e)
         {
+            
+            // Checking if expressionTB needs to be cleared
+            checkExpression();
+            
             var btnClicked = (Button)sender;
 
             if (displayTB.Text.Equals("0"))
@@ -46,7 +51,7 @@ namespace Calculator
             justNumbersStr = displayTB.Text.Replace(".", "");
 
             
-            if (justNumbersStr.Length < MAX_LENGTH )
+            if (justNumbersStr.Length < MAX_DIGITS )
             {
                 displayTB.AppendText(btnClicked.Text);
             }
@@ -58,17 +63,26 @@ namespace Calculator
 
         private void ceBtn_Click(object sender, EventArgs e)
         {
+            // Checking if expressionTB needs to be cleared
+            checkExpression();
+
             displayTB.Text = "0";
         }
 
         private void cBtn_Click(object sender, EventArgs e)
         {
+            // Checking if expressionTB needs to be cleared
+            checkExpression();
+
             expressionTB.Clear();
             displayTB.Text = "0";
         }
 
         private void backspaceBtn_Click(object sender, EventArgs e)
         {
+            // Checking if expressionTB needs to be cleared
+            checkExpression();
+
             // Removing the last character
             displayTB.Text = displayTB.Text.Remove(displayTB.Text.Length - 1);
 
@@ -82,7 +96,7 @@ namespace Calculator
         {
             var btnPressed = (Button)sender;
             
-            if (Regex.IsMatch(expressionTB.Text, "^-?\\d+\\.?\\d*[+\\−×\\÷]$"))
+            if (Regex.IsMatch(expressionTB.Text, "^-?\\d+\\.?\\d*(E[\\+\\-]\\d+)?[+\\−×\\÷]$"))
             {
                 // Removing the last character
                 expressionTB.Text = expressionTB.Text.Remove(expressionTB.Text.Length - 1);
@@ -103,6 +117,9 @@ namespace Calculator
 
         private void negateBtn_Click(object sender, EventArgs e)
         {
+            // Checking if expressionTB needs to be cleared
+            checkExpression();
+
             var num = double.Parse(displayTB.Text);
             displayTB.Text = (num * -1).ToString();
         }
@@ -111,13 +128,16 @@ namespace Calculator
         {
             // Checking if it already contains a decimal
             if (displayTB.Text.Contains(".")) {return;}
+            
+            // Checking if expressionTB needs to be cleared
+            checkExpression();
 
             // Removing the negative and decimal from the number to get the number of digits
             var justNumbersStr = displayTB.Text.Replace("-", "");
             justNumbersStr = displayTB.Text.Replace(".", "");
             
             // Checking number of digits
-            if (justNumbersStr.Length < MAX_LENGTH)
+            if (justNumbersStr.Length < MAX_DIGITS)
             {
                 displayTB.AppendText(".");
             }
@@ -127,14 +147,21 @@ namespace Calculator
         private void equalsBtn_Click(object sender, EventArgs e)
         {
             // Checking that a number and operator have been entered using regex
-            if (!Regex.IsMatch(expressionTB.Text, "^-?\\d+\\.?\\d*[+\\−×\\÷]$")) { return; }
-            
-            
+            if (!Regex.IsMatch(expressionTB.Text, "^-?\\d+\\.?\\d*(E[\\+\\-]\\d+)?[+\\−×\\÷]$")) { return; }
 
             _rightNum = double.Parse(displayTB.Text);
 
-            displayTB.Text = CalculatorOperation.Calculate(_leftNum, _rightNum, _operator).ToString();
-            
+            string result = CalculatorOperation.Calculate(_leftNum, _rightNum, _operator).ToString();
+            if (result.Length > MAX_STR_LENGTH)
+            {
+                while (result.Length - MAX_STR_LENGTH > 0)
+                {
+                    result = Regex.Replace(result, "\\dE", "E");
+                }
+            }
+
+            displayTB.Text = result;
+
             expressionTB.AppendText(_rightNum + "=");
 
         }
@@ -144,6 +171,11 @@ namespace Calculator
             if (expressionTB.Text.Contains("="))
             {
                 expressionTB.Clear();
+            }
+
+            if (displayTB.Text.Contains("E"))
+            {
+                displayTB.Text="0";
             }
         }
         
